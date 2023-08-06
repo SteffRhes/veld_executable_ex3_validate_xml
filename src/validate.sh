3f9f865044471938a95a4c0b10bcd0c3bc1b5bd8
steffres <stefan.resch@oeaw.ac.at>
2023-08-06 23:13:21 +0200

    work on better dummy repos

--- src/validate.sh
+++ src/validate.sh
@@ -1,23 +1,29 @@
 input_xml=/veld/input/a/data.xml
 input_xsd=/veld/input/b/schema.xsd
-output_txt=/veld/output/result.txt
+output_sh=/veld/output/result.sh
 
 if [ -f /veld/input/b/schema.xsd ]; then
   output=$(xmllint --schema ${input_xsd}  ${input_xml} --noout 2>&1)
   if [[ $output == *"validates" ]]; then
-    result="valid, with xsd check";
+    xml_is_valid="true";
+    xsd_was_used="true";
   else
-    result="invalid, with xsd check";
+    xml_is_valid="false";
+    xsd_was_used="true";
   fi
 else 
   output=$(xmllint --noout ${input_xml} 2>&1)
   if [ -z "$output" ]; then
-    result="valid xml, without xsd check"; 
+    xml_is_valid="true";
+    xsd_was_used="false";
   else
-    result="invalid xml, without xsd check"; 
+    xml_is_valid="false";
+    xsd_was_used="false";
   fi
 fi
 
-echo $result
-echo $result > $output_txt
+echo "# reuse these variables in bash by sourcing it" > $output_sh
+echo "xml_is_valid: ${xml_is_valid}"
+echo "xsd_was_used: ${xsd_was_used}"
+typeset -p xml_is_valid xsd_was_used >> $output_sh
 
